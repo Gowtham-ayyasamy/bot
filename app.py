09c252985520be81b86cd080398c64efa541c629
Gowtham-ayyasamy <gowtham.ayyasamy@emergertech.com>
2025-07-06 17:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
-from telegram.ext import Updater, MessageHandler, Filters
+# from telegram.ext import Updater, MessageHandler, Filters
+# import os
+
+
+
+# def forward_message(update, context):
+#     from_chat_id = update.effective_chat.id
+#     message = update.effective_message
+
+#     if from_chat_id == GROUP_A_ID:
+#         context.bot.send_message(chat_id=GROUP_B_ID, text=message.text)
+#     elif from_chat_id == GROUP_B_ID:
+#         context.bot.send_message(chat_id=GROUP_A_ID, text=message.text)
+
+# updater = Updater(TOKEN, use_context=True)
+# dp = updater.dispatcher
+# dp.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_message))
+# updater.start_polling()
+# updater.idle()
+
+
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
 TOKEN = '7542836224:AAFa68zmbXLnfDaTKdJ8DoiFZbO1-jWEvmk'
 GROUP_A_ID = '-4621356734'
 GROUP_B_ID = '-4957496536'
-
-def forward_message(update, context):
+async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     from_chat_id = update.effective_chat.id
     message = update.effective_message
 
     if from_chat_id == GROUP_A_ID:
-        context.bot.send_message(chat_id=GROUP_B_ID, text=message.text)
+        await context.bot.send_message(chat_id=GROUP_B_ID, text=message.text)
     elif from_chat_id == GROUP_B_ID:
-        context.bot.send_message(chat_id=GROUP_A_ID, text=message.text)
+        await context.bot.send_message(chat_id=GROUP_A_ID, text=message.text)
 
-updater = Updater(TOKEN, use_context=True)
-dp = updater.dispatcher
-dp.add_handler(MessageHandler(Filters.text & ~Filters.command, forward_message))
-updater.start_polling()
-updater.idle()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), forward_message))
+
+print("🚀 Bot is running...")
+app.run_polling()
